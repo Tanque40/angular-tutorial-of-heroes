4f3260f7fc211593270148297a12207206e6a2f1
Bruno Vitte <bruno_vitte@hotmail.com>
2022-06-20 17:01:44 +0000

    The hero editor

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+// DIferent imports
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+// Components 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
