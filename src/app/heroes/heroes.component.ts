8abb07c8b1ae03d38bb7a052a44fd8194148007f
Bruno Vitte <bruno_vitte@hotmail.com>
2022-06-20 17:21:32 +0000

    Display a selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+
+// Variables
+import { HEROES } from '../mock-heroes';
  
 @Component({
   selector: 'app-heroes',
@@ -13,9 +16,16 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
   
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
   hero: Hero = {
     id: 1,
     name: 'Windstorm',
   };
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
